5c4b5f58f38a07e1b471b14d2161d28131b15e89
ShivJee-Yadav <shivyadav1936@gmail.com>
2025-12-18 14:32:23 +0530

    Uploaded Balanced Data

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import pandas as pd
 
 st.title("Space Traffic Management , Find Collision Probability and Risk Factor")
 st.header("Space Traffic Management")
+
 st.markdown("Markdown ")
-
-
 st.write("The value of :red[***x***] is : ", 12)
+sample_df = pd.read_csv('data/sample_data.csv')
+    
+with st.expander('Sample Data'):
+    sample_df.shape
+    
+    sample_df
 
 col1, col2 = st.columns(2)
 with col1:
